e962d95f6021cea43f684e10d597bea5976f1003
preetikrp <preetikrp@gmail.com>
2017-04-11 15:29:57 -0500

    finished the sonnet

---
+++ sonnet.js
@@ -0,0 +1,8 @@
+var sonnet=
+document.getElementById('sonnet').innerHTML;
+var index= sonnet.index0f('orphans');
+console.log('index of orphans', index');
+var length =sonnet.length
+console.log('length of sonnet',length);
+sonnet = sonnet.replace('winter', 'yuletide');
+console.log(sonnet);
